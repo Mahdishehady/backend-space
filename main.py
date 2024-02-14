92a4b07b24d4f32c0195b9d55f56525553b3668f
Mahdi Shehadi <mahdishehadi52@gmail.com>
2024-02-14 04:13:28 +0200

    new commit

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query ,HTTPException
 from starlette.middleware.cors import CORSMiddleware
 
 from constants import Constants
 from model.database import Database
 from pointClass.PointClass import Point
 from services.pointService import PointService
+from typing import Dict , List
+from pydantic import BaseModel
 
+
+class DataPoint(BaseModel):
+    bs: str
+    hdbs: str
+    tbs: str
+    fs: str
+    hdfs: str
+    tfs: str
+
+
+class DataRequest(BaseModel):
+    points: Dict[str, DataPoint]
 app = FastAPI()
-# Allow all origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # Update this with your frontend domain
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
 )
 db = Database(Constants.HOST, Constants.USER, Constants.PORT, Constants.PASSWORD, Constants.DATABASE)
@@ -43,10 +56,16 @@ async def save_point(
         longminute: float = Query(...),
         longsecond: float = Query(...),
         geodeticheight: float = Query(...),
-        h: float = Query(...)
+        h: float = Query(...),
+        bs: float = Query(...),
+        hdbs: float = Query(...),
+        tbs: float = Query(...),
+        fs: float = Query(...),
+        hdfs: float = Query(...),
+        tfs: float = Query(...),
 ):
-    point: any = {
-        "Name": Name,
+    point = {
+        "Name": Name.upper(),
         "latdegree": latdegree,
         "latminute": latminute,
         "latsecond": latsecond,
@@ -54,29 +73,59 @@ async def save_point(
         "longminute": longminute,
         "longsecond": longsecond,
         "geodeticheight": geodeticheight,
-        "h": h
+        "h": h,
+        "bs": bs,
+        "hdbs": hdbs,
+        "tbs": tbs,
+        "fs": fs,
+        "hdfs": hdfs,
+        "tfs": tfs
     }
-    object =point_service.save_point(point)
-    return {
-        object
-    }
+    result = point_service.save_point(point)
+    if "already exists" in result:
+        raise HTTPException(status_code=409, detail=result)  # Conflict
+    else:
+        return {"message": result}
+
+@app.post("/api/savepairpoint")
+def save_pair_point(items: DataRequest):
+    return {"message": "Data received successfully"}
 
 
-@app.get("/fetch_data")
-def fetch_data():
-    try:
-        query = f"SELECT * FROM {Constants.TABLE}"
-        data = db.execute_query(query)
-        return data
-    except Exception as e:
-        return {"error": str(e)}
 
 
-@app.post("/insert_data")
-def insert_data(data: dict):
-    try:
-        query = f"INSERT INTO {Constants.TABLE} (column1, column2, ...) VALUES (%s, %s, ...)"
-        db.execute_insert(query, (data["value1"], data["value2"], ...))
-        return {"message": "Data inserted successfully"}
-    except Exception as e:
-        return {"error": str(e)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.get("/fetch_data")
+# def fetch_data():
+#     try:
+#         query = f"SELECT * FROM {Constants.TABLE}"
+#         data = db.execute_query(query)
+#         return data
+#     except Exception as e:
+#         return {"error": str(e)}
+#
+#
+# @app.post("/insert_data")
+# def insert_data(data: dict):
+#     try:
+#         query = f"INSERT INTO {Constants.TABLE} (column1, column2, ...) VALUES (%s, %s, ...)"
+#         db.execute_insert(query, (data["value1"], data["value2"], ...))
+#         return {"message": "Data inserted successfully"}
+#     except Exception as e:
+#         return {"error": str(e)}
--- services/pointService.py
+++ services/pointService.py
@@ -1,4 +1,3 @@
-# service.py
 import mysql.connector
 from mysql.connector import Error
 
@@ -23,25 +22,33 @@ class PointService:
             )
 
             if connection.is_connected():
-                # Define the SQL query
-                sql_query = """
-                INSERT INTO point (Name, latdegree, latminute, latsecond, longdegree, longminute, longsecond, geodeticheight, h)
-                VALUES (%(Name)s, %(latdegree)s, %(latminute)s, %(latsecond)s, %(longdegree)s, %(longminute)s, %(longsecond)s, %(geodeticheight)s, %(h)s)
-                """
-
                 # Create a cursor object
                 cursor = connection.cursor()
 
-                # Execute the query with the provided data
-                cursor.execute(sql_query, data)
+                # Check if the point already exists
+                sql_query_check = """
+                SELECT * FROM point WHERE Name = %(Name)s 
+                """
+                cursor.execute(sql_query_check, data)
+                existing_point = cursor.fetchone()
 
-                # Commit the transaction
-                connection.commit()
-
-                print("Data inserted successfully!")
+                if existing_point:
+                    return "Point already exists"
+                else:
+                    # Define the SQL query
+                    sql_query_insert = """
+                    INSERT INTO point (Name, latdegree, latminute, latsecond, longdegree, longminute, longsecond, geodeticheight, h, bs, hdbs, tbs, fs, hdfs, tfs)
+                    VALUES (%(Name)s, %(latdegree)s, %(latminute)s, %(latsecond)s, %(longdegree)s, %(longminute)s, %(longsecond)s, %(geodeticheight)s, %(h)s, %(bs)s, %(hdbs)s, %(tbs)s, %(fs)s, %(hdfs)s, %(tfs)s)
+                    """
+                    # Execute the query with the provided data
+                    cursor.execute(sql_query_insert, data)
+                    # Commit the transaction
+                    connection.commit()
+                    return "Data inserted successfully!"
 
         except Error as e:
             print("Error while connecting to MySQL", e)
+            return "Error while connecting to MySQL: {}".format(e)
 
         finally:
             # Close the connection
@@ -50,14 +57,38 @@ class PointService:
                 connection.close()
                 print("MySQL connection is closed")
 
-# sql_query = """
-#                INSERT INTO your_table (Name, latdegree, latminute, latsecond, longdegree, longminute, longsecond, geodeticheight, h)
-#                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-#                """
+    def get_point_details(self, name):
+        try:
+            # Establish connection to MySQL database
+            connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                port=self.port,
+                password=self.password,
+                database=self.database
+            )
 
-# data not point
-# cursor.execute(sql_query, (
-#     point.Name, point.latdegree, point.latminute, point.latsecond,
-#     point.longdegree, point.longminute, point.longsecond,
-#     point.geodeticheight, point.h
-# ))
+            if connection.is_connected():
+                # Create a cursor object
+                cursor = connection.cursor()
+
+                # Define the SQL query
+                sql_query_select = """
+                SELECT Name, bs, hdbs, tbs, fs, hdfs, tfs FROM point WHERE Name = %(Name)s 
+                """
+                # Execute the query with the provided data
+                cursor.execute(sql_query_select, {'Name': name})
+                # Fetch the result
+                point_details = cursor.fetchone()
+                return point_details
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+            return None
+
+        finally:
+            # Close the connection
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+                print("MySQL connection is closed")

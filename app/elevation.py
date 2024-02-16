59fe3814221509e1c2ae62f20151de349ec7769b
Mahdi Shehadi <mahdishehadi52@gmail.com>
2024-02-16 23:11:26 +0200

    removed mangum

---
+++ app/elevation.py
@@ -0,0 +1,56 @@
+from app.services.pointService import PointService
+from app.services.pairPointService import pairPointService
+from app.helpers.formulas import *
+from app.helpers.helpers import *
+
+pairPointService = pairPointService(host=Constants.HOST, user=Constants.USER, port=Constants.PORT,
+                                    password=Constants.PASSWORD,
+                                    database=Constants.DATABASE)
+point_service = PointService(host=Constants.HOST, user=Constants.USER, port=Constants.PORT, password=Constants.PASSWORD,
+                             database=Constants.DATABASE)
+
+
+def enter_data(pairname: str):
+    points = convertPairPoints_dict(pairPointService.get_points_by_pairname(pairname))
+    print(points)
+    return points
+
+def elevation(data):
+    curvature_dict = {}
+    rod_temp_corr_dict = {}
+    misclosure_dict = {}
+    elevation_difference = 100
+    for point, value in data.items():
+        bs_curvature = get_curvature(value['BS'], value['HDBS'])
+        fs_curvature = get_curvature(value['FS'], value['HDFS'])
+        bs_collimation = get_collimation(bs_curvature, value['HDBS'])
+        fs_collimation = get_collimation(fs_curvature, value['HDFS'])
+        elevation_difference = get_elevation_difference(elevation_difference, bs_collimation, fs_collimation)
+        curvature_dict[point] = {'Curvature BS': round(bs_curvature, 9),
+                                 'Curvature FS': round(fs_curvature, 9),
+                                 'Collimation BS': round(bs_collimation, 9),
+                                 'Collimation FS': round(fs_collimation, 9),
+                                 'Elevation Difference': round(elevation_difference, 8)}
+
+    print('-----------------------------------------------------------------------------------------------------')
+    mean_temp = get_mean_temp(get_BS_temp_list(data), get_FS_temp_list(data))
+    keys_list = list(curvature_dict.keys())  # Convert the keys to a list
+    main_elev_diff = 100 - curvature_dict[keys_list[int(len(curvature_dict) / 2) - 1]]['Elevation Difference']
+    temp_error = get_temp_error(mean_temp, main_elev_diff)
+    adj_elev_diff = get_temp_adj_diff_in_elevation(temp_error, main_elev_diff)
+    rod_temp_corr_dict['Rod Temp Corr Baseline'] = {'Mean Temp': round(mean_temp, 7),
+                                                    'Stand Temp': STANDARD_TEMP,
+                                                    'Diff in Elev': round(main_elev_diff, 8),
+                                                    'Thermal Expansion coefficient': THERMAL_EXPANSION_COEFFICIENT,
+                                                    'Temp-Error': temp_error,
+                                                    'Adj Temp Diff in Elev': round(adj_elev_diff, 8)}
+
+    print('-----------------------------------------------------------------------------------------------------')
+    misclosure_error = get_misclosure_error(get_sum_BS(data), get_sum_FS(data))
+    corrected_adj_diff_in_elev = get_misclosure_adj_diff_in_elevation(adj_elev_diff, misclosure_error)
+    misclosure_dict['Misclosure Baseline'] = {'Adj Temp Diff in Elev': round(adj_elev_diff, 8),
+                                              'Error': misclosure_error,
+                                              'Adj Diff in Elev': round(corrected_adj_diff_in_elev, 8)}
+
+    print('-----------------------------------------------------------------------------------------------------')
+    return misclosure_dict['Misclosure Baseline']['Adj Diff in Elev'], rod_temp_corr_dict, misclosure_dict
---
+++ app/main.py
@@ -0,0 +1,140 @@
+from fastapi import FastAPI, Query, HTTPException
+from starlette.middleware.cors import CORSMiddleware
+
+from app.model.database import Database
+from app.services.pairPointService import pairPointService
+from app.calculationService import calc_add_more
+from app.patent import *
+from mangum import Mangum
+
+class LevellingData(BaseModel):
+    H_levelling_m: float
+
+
+class CalcDataTableParams(BaseModel):
+    levelling: Dict[str, LevellingData]
+    startPoint: str
+    endPoint: str
+
+
+class DataPoint(BaseModel):
+    bs: str
+    hdbs: str
+    tbs: str
+    fs: str
+    hdfs: str
+    tfs: str
+
+
+class DataRequest(BaseModel):
+    points: Dict[str, DataPoint]
+
+
+app = FastAPI()
+handler=Mangum(app)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Update this with your frontend domain
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+db = Database(Constants.HOST, Constants.USER, Constants.PORT, Constants.PASSWORD, Constants.DATABASE)
+point_service = PointService(host=Constants.HOST, user=Constants.USER, port=Constants.PORT, password=Constants.PASSWORD,
+                             database=Constants.DATABASE)
+pairPointService = pairPointService(host=Constants.HOST, user=Constants.USER, port=Constants.PORT,
+                                    password=Constants.PASSWORD,
+                                    database=Constants.DATABASE)
+
+
+@app.on_event("startup")
+async def startup_event():
+    db.connect()
+    print("connected")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    db.disconnect()
+
+
+@app.get("/")
+def hello():
+    return {"Welcome To Аномалия высоты API's"}
+
+
+@app.get("/api/save-point")
+async def save_point(
+        Name: str = Query(...),
+        latdegree: float = Query(...),
+        latminute: float = Query(...),
+        latsecond: float = Query(...),
+        longdegree: float = Query(...),
+        longminute: float = Query(...),
+        longsecond: float = Query(...),
+        geodeticheight: float = Query(...),
+        h: float = Query(...)
+):
+    point = {
+        "Name": Name.upper(),
+        "latdegree": latdegree,
+        "latminute": latminute,
+        "latsecond": latsecond,
+        "longdegree": longdegree,
+        "longminute": longminute,
+        "longsecond": longsecond,
+        "geodeticheight": geodeticheight,
+        "h": h
+    }
+    result = point_service.save_point(point)
+    if "already exists" in result:
+        raise HTTPException(status_code=409, detail=result)  # Conflict
+    else:
+        return {"message": result}
+
+
+@app.post("/api/savepairpoint")
+def save_pair_point(items: DataRequest):
+    print("hi")
+    first_pair_name = (next(iter(items.points.keys()), None)).upper()
+    if first_pair_name:
+        result = pairPointService.save_pair_point(first_pair_name)
+        if result != "Pair point inserted successfully!":
+            return {"error": f"Error while saving pair point {first_pair_name}: {result}"}
+            # Iterate over remaining keys and save them with the first pairName
+        for pair_name1, point_data in items.points.items():
+            pair_name = pair_name1.upper()
+            if pair_name != first_pair_name:
+                result_pair_values = pairPointService.save_pair_values(first_pair_name, pair_name, point_data)
+    else:
+        raise HTTPException(status_code=404, detail="No points found")
+
+    return {"message": "Pair points and their values saved successfully!"}
+
+
+class PairPointsResponse(BaseModel):
+    points: dict
+
+
+# API endpoint to get data about a pair name
+@app.post("/api/get-data-table")
+def get_pair_points(DataTableParams: CalcDataTableParams):
+    datatable = calc_add_more(DataTableParams.levelling, DataTableParams.startPoint, DataTableParams.endPoint)
+    print("final table")
+    print( datatable)
+    return {"data": datatable}
+
+
+@app.post("/api/getNLevelling")
+def get_NLevelling(levelling: Dict[str, LevellingData]):
+    getNLevelling= get_Geoid_Undulation_NLevelling(levelling)
+    return getNLevelling
+@app.post("/api/getNEGM2008")
+def get_NEGM2008(levelling: Dict[str, LevellingData]):
+    getNEGM2008= get_Geoid_Undulation_NEGM2008(levelling)
+    return getNEGM2008
+
+@app.post("/api/getmeandeviation")
+def getMeanDeviation(levelling: Dict[str, LevellingData]):
+    getmeandeviationdata= get_mean_deviation(levelling)
+    return getmeandeviationdata
